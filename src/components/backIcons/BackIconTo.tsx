82d9e49779d030d84737970716254c9fedde92f7
nateazevedo <nathanielazevedo@hotmail.com>
2025-05-14 15:40:40 -0400

    fixes build - ts error

--- src/components/backIcons/BackIconTo.tsx
+++ src/components/backIcons/BackIconTo.tsx
@@ -1,42 +1,42 @@
-import { Box, Button, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
-import { setSwipeDirection } from '../../state'
-import { useAppDispatch } from '../../state/redux'
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
+import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { setSwipeDirection } from "../../state";
+import { useAppDispatch } from "../../state/redux";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const BackIcon = ({ to }: { to: string }) => {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   return (
     <Box
       sx={{
-        top: '10px',
-        left: '0px',
-        width: '100%',
-        height: '50px',
-        display: 'flex',
-        alignItems: 'center',
-        position: 'absolute',
-        justifyContent: 'space-between',
+        top: "10px",
+        left: "0px",
+        width: "100%",
+        height: "50px",
+        display: "flex",
+        alignItems: "center",
+        position: "absolute",
+        justifyContent: "space-between",
       }}
     >
       <Button
         onClick={() => {
-          navigate(to)
-          dispatch(setSwipeDirection('right'))
+          navigate(to);
+          dispatch(setSwipeDirection("right"));
         }}
         sx={styles.c}
       >
-        <ArrowBackIosNewIcon sx={{ fontSize: 10, mr: '3px' }} />
+        <ArrowBackIosNewIcon sx={{ fontSize: 10, mr: "3px" }} />
         Back
       </Button>
       {/* <Typography color='secondary'>Chicken Tinder</Typography> */}
     </Box>
-  )
-}
+  );
+};
 
-export default BackIcon
+export default BackIcon;
 
 const styles = {
   c: {
@@ -47,4 +47,4 @@ const styles = {
     // top: '10px',
     // left: '0px',
   },
-}
+};
